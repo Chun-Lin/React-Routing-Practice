bc9a7fc5690c53f4932544ce06e78847fe8b60cc
Chun-Lin <wulin40063@gmail.com>
2018-05-02 16:22:05 +0800

    feat: add react-router to blog and give styled to Link component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import axios from '../../axios'
+// import axios from '../../axios'
+import { Route, NavLink, withRouter } from 'react-router-dom'
 
 import './Blog.css'
 import Posts from '../Posts/Posts'
+import NewPost from '../NewPost/NewPost'
 
 class Blog extends Component {
   render() {
@@ -12,15 +14,26 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <NavLink to="/" exact>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit',
+                  }}
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     )
   }
